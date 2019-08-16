a5615f9c346237b87663b1adb8d36e35c45e2bb1
Dushyant Pathak <dushyantpathak6789@gmail.com>
2019-08-16 21:21:00 +0530

    Create HeronsFormula.py

---
+++ HeronsFormula.py
@@ -0,0 +1,19 @@
+def compute_area(sides):
+  # slice the input to get the sides
+  a = sides[:,0]  # 5.0, 2.3
+  b = sides[:,1]  # 3.0, 4.1
+  c = sides[:,2]  # 7.1, 4.8
+  
+  # Heron's formula
+  s = (a + b + c) * 0.5   # (a + b) is a short-cut to tf.add(a, b)
+  areasq = s * (s - a) * (s - b) * (s - c) # (a * b) is a short-cut to tf.multiply(a, b), not tf.matmul(a, b)
+  return tf.sqrt(areasq)
+
+with tf.Session() as sess:
+  # pass in two triangles
+  area = compute_area(tf.constant([
+      [5.0, 3.0, 7.1],
+      [2.3, 4.1, 4.8]
+    ]))
+  result = sess.run(area)
+  print(result)
